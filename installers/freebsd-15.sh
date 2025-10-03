9119af2be8b37419b31465484bd87e8bcf2871ab
Dick Johnson <einheit@mainphrame.com>
2025-10-03 12:16:19 -0700

    installer cleanup, new installers tested

---
+++ installers/freebsd-15.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# FreeBSD 15 installer
+#
+
+echo 
+echo "This install script is for FreeBSD 15 and a mysql DB"
+echo "if using postgresql or other DB, you'll need to manually"
+echo "edit the maia/maiad config files & the php config file"
+echo 
+
+
